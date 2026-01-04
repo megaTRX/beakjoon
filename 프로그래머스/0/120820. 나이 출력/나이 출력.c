9cffcb7e12f9a0add2540f36d7f9bb5e05bb8b93
Daeyeong Choi <129028297+megaTRX@users.noreply.github.com>
2026-01-04 13:20:51 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.36 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int age) {
+    int answer = 0;
+    int year = 2023;
+
+    answer = year - age;
+    return answer;
+}
