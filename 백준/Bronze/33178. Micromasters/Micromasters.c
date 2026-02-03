3257d38d2cc092fdc2b60f9a91173977c10c2fd1
Daeyeong Choi <129028297+megaTRX@users.noreply.github.com>
2026-02-03 10:34:39 +0900

    [Bronze V] Title: Micromasters, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/33178. Micromasters/Micromasters.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable: 4996)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+	int N;
+	scanf("%d", &N);
+	int num = (N / 10);
+
+	printf("%d", num);
+	
+	return 0;
+}
